3bc58edcdcdc77b83c46c81a74a43de2904d2377
Max Rockatansky <denisigoshev@gmail.com>
2018-05-04 16:23:36 +0300

    test - moving player... test passed nicely http://prntscr.com/jdock7

--- src/test/scala/ClientTest.scala
+++ src/test/scala/ClientTest.scala
@@ -1,7 +1,7 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.ws.{Message, TextMessage, WebSocketRequest}
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, OverflowStrategy}
 import akka.stream.scaladsl.{Keep, Sink, Source}
 import akka.stream.testkit.scaladsl.TestSink
 import org.scalatest.{FunSuite, Matchers}
@@ -32,15 +32,33 @@ class ClientTest extends FunSuite with Matchers {
   test("should be able to move player ") {
     val client = new Client("Dennis")
     // output = List[players]
-    val input = Source.empty[String]
+    val input = Source.actorRef[String](5,OverflowStrategy.dropNew)
     val output = TestSink.probe[List[Player]]
-    val result = client.run(input,output)
+    val ((inputMat,result),outputMat) = client.run(input,output)
+    inputMat ! "up"
+    outputMat.request(2)
+    outputMat.expectNext(List(Player("Dennis",Position(0,0))))
+    outputMat.expectNext(List(Player("Dennis",Position(0,2))))
 
   }
 }
 
-case class Client(playerName:String){
-  def run[M1,M2](input:Source[String,M1], output :Sink[List[Player], M2] ) = ???
+case class Client(playerName:String)(implicit val actorSystem:ActorSystem, implicit val
+actorMaterializer:ActorMaterializer){
+ import spray.json._
+  import DefaultJsonProtocol._
+  implicit val positionFormat = jsonFormat2(Position)
+  implicit val playerFormat = jsonFormat2(Player)
+  val webSocketFlow =
+    Http().webSocketClientFlow(WebSocketRequest(s"ws://localhost:8080/?playerName=$playerName")).collect{
+      case TextMessage.Strict(strMsg) => strMsg.parseJson.convertTo[List[Player]]
+    }
+  def run[M1,M2](input:Source[String,M1], output :Sink[List[Player], M2] )= {
+    input.map(direction => TextMessage(direction) )
+      .viaMat(webSocketFlow)(Keep.both) // keep the materialized Future[WebSocketUpgradeResponse]
+      .toMat(output)(Keep.both) // also keep the Future[Done]
+      .run()
+  }
 }
 case class Player(name:String, position:Position)
 case class Position(x:Int , y:Int )
